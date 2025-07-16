f9e5933c3530098a059216a7978691c6ffc8fafc
JongGun Lee <jglee.dlab@gmail.com>
2025-07-16 11:05:09 +0900

    entripoint.sh 파일 업데이트

--- ansible/ansible-client/entrypoint.sh
+++ ansible/ansible-client/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # authorized_keys 파일 설정
+# 주의 : 이파일은 반드시 LF로 라인 줄바꿈이 되어 있어야 함 (CRLF 일 경우 실행 오류 발생)
 cat /home/ansible_user/id_rsa.pub > /home/ansible_user/.ssh/authorized_keys
 chown ansible_user:ansible_user /home/ansible_user/.ssh/authorized_keys
 chmod 600 /home/ansible_user/.ssh/authorized_keys
